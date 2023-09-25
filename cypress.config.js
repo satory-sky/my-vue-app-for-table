b5a748b99bd2ca2aaba8cb520bbaaaa10f3659d1
akontare <alex.kontarero-v@barrenjoey.com>
2023-09-25 13:54:36 +1000

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 8000,
+  chromeWebSecurity: false, // Disabled to prevent errors with iframes. Would need to reenable if checking for CORS errors in the future.
   component: {
     specPattern: "src/**/*spec.{js,jsx,ts,tsx}",
     devServer: {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue, { createApp } from '@vue/compat';
+import { createApp } from '@vue/compat';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue);
 const app = createApp(App);
+app.use(BootstrapVue);
 app.mount('#app');
